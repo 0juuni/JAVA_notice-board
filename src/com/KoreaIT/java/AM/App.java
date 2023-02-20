a8b1ac40f35f352c9a1dfccb82dedf3f203862bf
yeoungjun0226 <gimyeongjun0226@gmail.com>
2023-02-20 09:55:56 +0900

    테스트 데이터 생성 메소드 구현, 리팩토링, 클래스/패키지 정리, 메인로직 APP으로 이전

--- src/com/KoreaIT/java/AM/App.java
+++ src/com/KoreaIT/java/AM/App.java
@@ -1,17 +1,29 @@
+package com.KoreaIT.java.AM;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-	
-	static List<Article> articles = new ArrayList<>();
-	
-	public static void main(String[] args) {
+import com.KoreaIT.java.AM.dto.Article;
+import com.KoreaIT.java.AM.util.Util;
+
+public class App {
+
+	public static List<Article> articles;
+
+	static {
+		articles = new ArrayList<>();
+	}
+
+	public void run() {
 		System.out.println("==프로그램 시작==");
-		Scanner sc = new Scanner(System.in);
-		int lastArticleId = 0;
+
 		makeTestData();
-		 
+
+		Scanner sc = new Scanner(System.in);
+
+		int lastArticleId = 3;
+
 		while (true) {
 			System.out.printf("명령어 ) ");
 			String command = sc.nextLine().trim();
@@ -39,7 +51,7 @@ public class Main {
 			} else if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
-				String regDate = date.getNowDateTimeStr();
+				String regDate = Util.getNowDateTimeStr();
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
@@ -69,14 +81,49 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-				
+
 				foundArticle.increaseHit();
-				
+
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
-				System.out.printf("조회수 :  %d\n", foundArticle.hit);
+				System.out.printf("조회수 : %d\n", foundArticle.hit);
+
+			} else if (command.startsWith("article modify ")) {
+
+				String[] cmdBits = command.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+				//int foundIndex = -1;
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						//foundIndex = i;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				foundArticle.title = title;
+				foundArticle.body = body;
+				/* 2. Article 자체를 덮어씌우는 방식 - 위 방식이 좀더 효율적
+				Article article2 = new Article(id, regDate, title, body);
+				articles.set(foundIndex, article2); */
+				
+				System.out.printf("%d번 글을 수정했습니다.\n", id);
 
 			} else if (command.startsWith("article delete ")) {
 
@@ -101,43 +148,10 @@ public class Main {
 				// size() -> 3
 				// index : 0, 1, 2
 				// id : 1, 2, 3
-
+				
 				articles.remove(foundIndex);
 				System.out.printf("%d번 글을 삭제했습니다.\n", id);
 
-			} else if (command.startsWith("article modify")) {
-				String[] cmdBits = command.split(" ");
-				int id = Integer.parseInt(cmdBits[2]);
-				//int foundIndex = -1;
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.id == id) {
-						foundArticle = article;
-						//foundIndex = i;
-						break;
-					}
-				}
-
-				if (foundArticle == null) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
-					continue;
-				}
-
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
-				
-				foundArticle.title = title;
-				foundArticle.body = body;
-				
-				/* 2. Article 자체를 덮어씌우는 방식 - 위 방식이 좀더 효율적
-				Article article2 = new Article(id, regDate, title, body);
-				articles.set(foundIndex, article2); */
-
-				System.out.println(id + "번 글이 수정되었습니다");
 			}
 
 			else {
@@ -148,28 +162,13 @@ public class Main {
 		System.out.println("==프로그램 끝==");
 
 		sc.close();
-	}
-	static void makeTestData() {
-		System.out.println("테스트를 위한 데이터를 생성합니다.");
-	}
-}
 
-class Article {
-	int id;
-	String regDate;
-	String title;
-	String body;
-	int hit;	
-	
-	Article(int id, String regDate, String title, String body) {
-		this.id = id;
-		this.regDate = regDate;
-		this.title = title;
-		this.body = body;
-		this.hit = 0;
 	}
-	
-	void increaseHit() {
-		this.hit++;
+
+	static void makeTestData() {
+		System.out.println("테스트를 위한 데이터를 생성합니다");
+		articles.add(new Article(1, Util.getNowDateTimeStr(), "제목1", "내용1", 11));
+		articles.add(new Article(2, Util.getNowDateTimeStr(), "제목2", "내용2", 22));
+		articles.add(new Article(3, Util.getNowDateTimeStr(), "제목3", "내용3", 33));
 	}
 }
--- src/com/KoreaIT/java/AM/util/Util.java
+++ src/com/KoreaIT/java/AM/util/Util.java
@@ -1,7 +1,9 @@
+package com.KoreaIT.java.AM.util;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class date {
+public class Util {
 	/** 현재 날짜, 시각 정보 String 리턴 */
 	public static String getNowDateTimeStr() {
 		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
